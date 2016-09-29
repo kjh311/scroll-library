27a62b7ed8bdd64ed54a8d73d28e171e23607253
Kevin Huelsmann <kjh311@gmail.com>
2016-09-28 19:02:58 -0700

    viewport is devide width

--- scroll.js
+++ scroll.js
@@ -1,19 +1,84 @@
 $( document ).ready(function() {
 
-
-
+  $(window).scroll(function () {
+    $("body").css("background-position","50% " + ($(this).scrollTop() / 2) + "px");
+});
 
     $(function(){
-      var bounce = 'animated bounce';
-      var rollIn = "animated rollIn";
-      var vendor = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      var bounce          = 'animated bounce';
+      var rollIn          = "animated rollIn";
+      var bounceInDown    = "animated bounceInDown";
+      var vendor          = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      var flip            = 'animated flip';
+      var flipInX         = 'animated flipInX';
+      var lightSpeedIn    = "animated lightSpeedIn";
+      var zoomIn          = "animated zoomIn";
+      var bounceOutLeft   = "animated bounceOutLeft";
+      var bounceOutDown   = "animated bounceOutDown";
+      var shake           = "animated shake";
+      var bounceInRight   = "animated bounceInRight";
+      var zoomOutDown     = "animated zoomOutDown";
+      var hinge           = "animated hinge";
 
 
-            $('.divy').on("click", function(){
-              $('.divy').addClass(rollIn).one(vendor, function(){
-                $(this).removeClass(rollIn);
+
+        $('.divy').on("click", function(){
+              $('.divy').addClass(shake).one(vendor, function(){
+                $(this).removeClass(shake);
               });
         });
+
+        $('#banner').waypoint(function(){
+            $('#banner').removeClass('no-opacity').addClass(flipInX).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'70%'})
+
+        $('#face').waypoint(function(){
+            $('#face').removeClass('no-opacity').addClass(lightSpeedIn).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'50%'})
+
+        $('#cat').waypoint(function(){
+            $('#cat').removeClass('no-opacity').addClass(zoomIn).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'10%'})
+
+        $('#girl').waypoint(function(){
+            $('#girl').removeClass('no-opacity').addClass(bounceInRight).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'70%'})
+
+        $('#monkey').waypoint(function(){
+            $('#monkey').removeClass('no-opacity').addClass(hinge).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'50%'})
+
+        $('#fish').waypoint(function(){
+            $('#fish').addClass(bounceOutLeft).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'10%'})
+
+        $('#penguin').waypoint(function(){
+            $('#penguin').addClass(bounceOutDown).one(vendor, function(){
+              // $(this).removeClass(flip);
+            });
+        },{offset:'10%'})
+
+
+        $('#divy2').waypoint(function(){
+            $('#divy2').removeClass('no-opacity').addClass(rollIn).one(vendor, function(){
+              $(this).removeClass(rollIn);
+            });
+        },{offset:'50%'})
+
+
+
 
 
 //         var waypoint = new Waypoint({
@@ -29,14 +94,6 @@ $( document ).ready(function() {
 //     alert('You have scrolled to divy2')
 //   }
 // })
-
-        $('#divy2').waypoint(function(){
-            $('#divy2').removeClass('no-opacity').addClass(rollIn).one(vendor, function(){
-              $(this).removeClass(rollIn);
-            });
-        },{offset:'50%'})
-
-
     });
 
 
